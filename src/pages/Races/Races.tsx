6d6424a6924fdafccbd996cf79c06becee4bb781
longhp18 <hoanglonguss1805@gmail.com>
2023-06-24 23:24:36 +0700

    Update Race: Searchbar

--- src/pages/Races/Races.tsx
+++ src/pages/Races/Races.tsx
@@ -1,12 +1,15 @@
-import type { ColumnsType } from "antd/es/table";
-
+import type { ColumnType, ColumnsType } from "antd/es/table";
+import { SearchOutlined } from "@ant-design/icons";
 import "./Races.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+
 import { useSelector } from "react-redux";
 
 import dayjs from "dayjs";
 import { RootState } from "../../redux/store";
 import TableList from "../../conponents/Table/TableList";
+import { Button, Input, InputRef, Space } from "antd";
+import { FilterConfirmProps } from "antd/es/table/interface";
 
 const Race = () => {
   const [raceData, setRaceData] = useState<any>();
@@ -27,11 +30,89 @@ const Race = () => {
     fetchYearResult(yearFilter);
   }, [dataApi, yearFilter]);
 
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef<InputRef>(null);
+
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: (param?: FilterConfirmProps) => void,
+    dataIndex: any
+  ) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText("");
+  };
+
+  const getColumnSearchProps = (dataIndex: any): ColumnType<any> => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            handleSearch(selectedKeys as string[], confirm, dataIndex)
+          }
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() =>
+              handleSearch(selectedKeys as string[], confirm, dataIndex)
+            }
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+  });
+
   const columns: ColumnsType<any> = [
     {
       title: "GRAND PRIX",
       dataIndex: "grand_prix",
       key: "grand_prix",
+      ...getColumnSearchProps("grand_prix"),
     },
     {
       title: "DATE",
